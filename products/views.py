939e7a882a79527588a714c02fa54742b02836f2
Stuart Saint <stuartsaint242@gmail.com>
2021-03-05 18:39:05 +0000

    Addition: category filtering

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 
 def all_products(request):
@@ -11,12 +11,19 @@ def all_products(request):
     # below ensures that there isn't an error whenever
     # the page is loaded without a search term
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't enter any search criteria")
+                messages.error(
+                     request, "You didn't enter any search criteria")
                 return redirect(reverse('products'))
 
             # pipe character represents 'or'
@@ -27,6 +34,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
