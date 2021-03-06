b02e4d6294009718730531a84ca7a1ba453628b8
Stuart Saint <stuartsaint242@gmail.com>
2021-03-06 17:45:11 +0000

    Addition: add to bag functionality

--- bag/urls.py
+++ bag/urls.py
@@ -5,5 +5,6 @@ from . import views
 # . means to import from the current directory
 
 urlpatterns = [
-    path('', views.view_bag, name='view_bag')
+    path('', views.view_bag, name='view_bag'),
+    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
 ]
