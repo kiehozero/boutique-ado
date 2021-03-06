9242f35d9e6e51e7d477528786aadeaaa1123b3c
Stuart Saint <stuartsaint242@gmail.com>
2021-03-06 20:36:02 +0000

    Addition: product_detail quantity input JS

--- bag/views.py
+++ bag/views.py
@@ -1,23 +1,42 @@
 from django.shortcuts import render, redirect
 
+# Create your views here.
+
 
 def view_bag(request):
-    """ A view to display current bag items """
+    """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
 
 
 def add_to_bag(request, item_id):
-    """ Add a quantity of an item to the bag """
+    """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    # below checks firstly whether an added item has a size. If so, it then
+    # checks if that item is already present. If not, it adds the item. If it
+    # is already present, it then checks whether an item of that size has
+    # already been added. If so, it update the quantity, and if not it simply
+    # adds a new item/size SKU to the bag.
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
--- products/models.py
+++ products/models.py
@@ -1,15 +1,13 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Category(models.Model):
+
     class Meta:
         verbose_name_plural = 'Categories'
 
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(
-        max_length=254, null=True, blank=True)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -19,7 +17,6 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    # only name description and price are mandatory
     category = models.ForeignKey(
         'Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
