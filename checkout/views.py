1c1cce071432f85d3ab2e42ff9f1e8d2acd48aff
Stuart Saint <stuartsaint242@gmail.com>
2021-03-07 17:31:53 +0000

    Addition: Stripe setup, JS

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51ISPN5AbTLswkfEnRMl1vGz6pty9s4oBG0BmmrbfguKdw61pk8STD9g9VZEZHyZrGDGW8wSYu9kcxw1WeLIc4Zze00EPXtrtpH',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
