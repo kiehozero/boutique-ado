c3808eb0017f307baf6f67f4897d2a65311f6505
Stuart Saint <stuartsaint242@gmail.com>
2021-03-07 20:44:28 +0000

    Addition: payment route to checkout success

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
 
-    # created 
+    # created
     'home',
     'products',
     'bag',
--- checkout/urls.py
+++ checkout/urls.py
@@ -5,5 +5,9 @@ from . import views
 # . means to import from the current directory
 
 urlpatterns = [
-    path('', views.checkout, name='checkout')
+    path('', views.checkout, name='checkout'),
+    path(
+        'checkout_success/<order_number>',
+        views.checkout_success,
+        name='checkout_success'),
 ]
