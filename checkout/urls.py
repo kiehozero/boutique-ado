30a4d9c3a1bf1dbcead1986fd68b32e57d9cdded
Stuart Saint <stuartsaint242@gmail.com>
2021-03-08 18:26:06 +0000

    Addition: classes for webhook handling

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 
 # . means to import from the current directory
@@ -10,4 +11,5 @@ urlpatterns = [
         'checkout_success/<order_number>',
         views.checkout_success,
         name='checkout_success'),
+    path('wh/', webhook, name="webhook"),
 ]
