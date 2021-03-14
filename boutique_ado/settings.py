dca51ad86d4fb9d00f3187fde2974baae96f9602
Stuart Saint <stuartsaint242@gmail.com>
2021-03-14 16:21:55 +0000

    Email setup

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -219,4 +219,4 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASS = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_USER')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
